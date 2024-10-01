983b846b44f5ccca7d2f107dda983b902dd48b22
Riccardo Lazzari <153769506+Riccalaz@users.noreply.github.com>
2024-10-01 20:08:55 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -9,17 +9,17 @@ public class Main {
 		
 		System.out.print("inserisci n di persone: ");
 		
-		int T = myObj.nextInt();
-		thread[] th=new thread[T];
-		cp c=new cp();
+		int T = myObj.nextInt(); //inserimento numero thread
+		thread[] th=new thread[T]; //arrey di thread
+		cp c=new cp(); //contatore per gli ingressi
 		
 		for(int i=0;i<T;i++){
 			th[i]= new thread(c);
-			th[i].start();
+			th[i].start(); 
 		}
 		
 		while(true) {
-			System.out.println("persone in discoteca: "+ c.getc());
+			System.out.println("persone in discoteca: "+ c.getc()); //stampo a schermo il contatore
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException e) {
